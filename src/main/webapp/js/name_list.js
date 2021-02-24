728cafb31486576bfde3ff9874035f30c876e48e
Tristen Smith <tristen.e.smith@gmail.com>
2021-02-24 18:16:41 -0500

    Worked on assignment 4

--- src/main/webapp/js/name_list.js
+++ src/main/webapp/js/name_list.js
@@ -1,23 +1,77 @@
 // Main Javascript File
 
+function htmlSafe(data) {
+    return data.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;");
+}
+
+function formatPhoneNumber(phoneNumberString) {
+    // Strip all non-digits
+    // Use a regular expression. Match all non-digits \D
+    // and replace with an empty string.
+    let cleaned = phoneNumberString.replace(/\D/g, '');
+
+    // Are we left with 10 digits? This will return them in
+    // three groups. This: (\d{3}) grabs the first three digits \d
+    // The 'match' variable is an array. First is the entire match
+    // the next locations are each group, which are surrounded by
+    // () in the parenthesis.
+    let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+        return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+    }
+    return phoneNumberString;
+}
+
+function getJSDateFromSQLDate(sqlDate) {
+    // Strip non-digits
+    let cleaned = sqlDate.replace(/\D/g, '');
+    // Match and group
+    let match = cleaned.match(/^(\d{4})(\d{2})(\d{2})$/);
+    // Create a new Date object
+    let resultDate = new Date(match[1], match[2], match[3]);
+    return resultDate;
+}
+
+
+
 function updateTable() {
     // Here's where your code is going to go.
     console.log("updateTable called");
+
+    let url = "api/name_list_get";
+
+    $.getJSON(url, null, function(json_result) {
+            // json_result is an object. You can set a breakpoint, or print
+            // it to see the fields. Specifically, it is an array of objects.
+            // Here we loop the array and print the first name.
+            for (let i = 0; i < json_result.length; i++) {
+                // Print the first name
+                console.log(json_result[i].first, json_result[i].last);
+
+                bday = getJSDateFromSQLDate(json_result[i].birthday);
+                bdayString = bday.toLocaleDateString();
+                console.log(bday);
+
+                $('#datatable tbody').append('<tr><td>'
+                    +htmlSafe(json_result[i].first)
+                    +'</td><td>'
+                    +htmlSafe(json_result[i].last)
+                    +'</td></tr>'
+                    +htmlSafe(json_result[i].email)
+                    +'</td></tr>'
+                    +formatPhoneNumber(htmlSafe(json_result[i].phone))
+                    +'</td></tr>'
+                    +htmlSafe(bdayString)
+                    +'</td></tr>');
+
+            }
+            console.log("Done");
+        }
+    );
+
 }
 
-let url = "api/name_list_get";
 
-$.getJSON(url, null, function(json_result) {
-        // json_result is an object. You can set a breakpoint, or print
-        // it to see the fields. Specifically, it is an array of objects.
-        // Here we loop the array and print the first name.
-        for (let i = 0; i < json_result.length; i++) {
-            // Print the first name
-            console.log(json_result[i].first, json_result[i].last);
-        }
-        console.log("Done");
-    }
-);
 
 // Call your code.
 updateTable();
